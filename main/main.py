80a8795e7b9b48aabbce0f92761c2e0854cb25f5
Kehinde Olalusi <kehindeolalusi75@gmail.com>
2025-02-06 12:36:24 +0100

    Update main.py

--- main/main.py
+++ main/main.py
@@ -20,12 +20,10 @@ def get_symbols(asset_category: str) -> list[str]:
 
 
 @task
-def get_hist_start_end_dates(asset_category: str) -> str | dt.date:
+def get_hist_start_end_dates(asset_category: str) -> dt.date:
     try:
         prices = s3_el.extract("price_history", asset_category)
-        dates = (
-            duckdb.sql("SELECT date FROM prices ORDER BY date").df().squeeze()
-        )
+        dates = duckdb.sql("SELECT date FROM prices ORDER BY date").df().squeeze()
         start_date = dates.iloc[-1] + dt.timedelta(days=1)
         end_date = dt.datetime.now().date()
         return start_date, end_date
@@ -37,37 +35,26 @@ def get_hist_start_end_dates(asset_category: str) -> str | dt.date:
 
 @flow
 def etl(
-    asset_category: str | list[str] = ["fx", "sp_stocks"],
+    asset_category: str,
     symbols: list[str] | None = None,
     start_date: str | dt.date | None = None,
     end_date: str | dt.date | None = None,
     chunk_size: int = 500,
 ):
-    if isinstance(asset_category, str) and asset_category not in ["fx", "sp_stocks"]:
+    if asset_category not in ["fx", "sp_stocks"]:
         raise ValueError(
             f"'asset_category should be one of 'fx' or 'sp_stocks', got {asset_category}"
         )
 
-    if isinstance(asset_category, list):
-        for category in asset_category:
-            symbols = get_symbols(category) if not symbols else symbols
-            start_date, end_date = (
-                get_hist_start_end_dates(category)
-                if not (start_date and end_date)
-                else (start_date, end_date)
-            )
-            price_history.etl_bars(category, symbols, start_date, end_date, chunk_size)
-            symbols = None
-    else:
-        symbols = get_symbols(asset_category) if not symbols else symbols
-        start_date, end_date = (
-            get_hist_start_end_dates(asset_category)
-            if not (start_date and end_date)
-            else (start_date, end_date)
-        )
-        price_history.etl_bars(
-            asset_category, symbols, start_date, end_date, chunk_size
-        )
+    symbols = get_symbols(asset_category) if not symbols else symbols
+    start_date, end_date = (
+        get_hist_start_end_dates(asset_category)
+        if not (start_date and end_date)
+        else (start_date, end_date)
+    )
+    price_history.etl_bars(
+        asset_category, symbols, start_date, end_date, chunk_size
+    )
 
     create_markdown_artifact(
         f"""
@@ -79,4 +66,5 @@ def etl(
 
 
 if __name__ == "__main__":
-   etl()
+    etl("fx")
+    etl("sp_stocks")
