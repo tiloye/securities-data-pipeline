41fb4397a081164a70838115b96909ee8738bae8
Kehinde Olalusi <kehindeolalusi75@gmail.com>
2026-04-01 00:32:06 +0100

    Fix S&P constituent flags in stg_stock_symbols.sql

--- dw_transformer/models/staging/stg_stock_symbols.sql
+++ dw_transformer/models/staging/stg_stock_symbols.sql
@@ -5,8 +5,8 @@ select
     sector,
     industry,
     'Stock' as asset_type,
-    False as in_sp400,
-    False as in_sp500,
-    False as in_sp600,
+    in_sp400,
+    in_sp500,
+    in_sp600,
     cast(date_stamp as date) as date_stamp
 from {{ source("raw", "symbols_sp_stocks") }}
