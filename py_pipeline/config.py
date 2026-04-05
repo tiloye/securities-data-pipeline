43956b5e855d60e71c5b4e6b67873011b3d79a3b
Kehinde Olalusi <kehindeolalusi75@gmail.com>
2026-04-05 02:38:59 +0100

    Deploy flows with prefecy yaml file
    - Fixes requirements installation issue on prefect cloud.
    - Causes Block.aload error during deployment in cli but fixed by adding "_sync" parameter to prefect block save and load method calls.

--- py_pipeline/config.py
+++ py_pipeline/config.py
@@ -18,7 +18,7 @@ if ENV_NAME == "dev":
         aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
         aws_secret_access_key=os.environ["AWS_SECRET_KEY"],
     )
-    aws_credentials.save(PREFECT_AWS_KEY_BLOCK, overwrite=True)
+    aws_credentials.save(PREFECT_AWS_KEY_BLOCK, overwrite=True, _sync=True)
 
     dw_secret = Secret(
         value={
@@ -28,10 +28,10 @@ if ENV_NAME == "dev":
             "port": os.getenv("DB_PORT"),
         }
     )
-    dw_secret.save(PREFECT_DW_CREDENTIALS_BLOCK, overwrite=True)
+    dw_secret.save(PREFECT_DW_CREDENTIALS_BLOCK, overwrite=True, _sync=True)
 
 # Load s3 credentials
-aws_credentials = AwsCredentials.load(PREFECT_AWS_KEY_BLOCK)
+aws_credentials = AwsCredentials.load(PREFECT_AWS_KEY_BLOCK, _sync=True)
 AWS_ACCESS_KEY = aws_credentials.aws_access_key_id
 AWS_SECRET_KEY = aws_credentials.aws_secret_access_key.get_secret_value()
 S3_ENDPOINT = os.environ["S3_ENDPOINT"]
@@ -39,7 +39,7 @@ BUCKET_NAME = os.environ["BUCKET_NAME"]
 DATA_PATH = f"s3://{BUCKET_NAME}"
 
 # Load database connection
-dw_credentials = Secret.load(PREFECT_DW_CREDENTIALS_BLOCK).get()
+dw_credentials = Secret.load(PREFECT_DW_CREDENTIALS_BLOCK, _sync=True).get()
 DB_TYPE = os.environ["DB_TYPE"]
 DB_HOST = dw_credentials.get("host")
 DB_PORT = dw_credentials.get("port")
